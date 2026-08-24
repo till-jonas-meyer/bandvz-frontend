64ae615272c5e9e1383c9f95d262f3aaf3bc8b2e
Till Jonas Meyer <weaveit@gmx.net>
2026-08-24 11:08:37 +0200

    Work on tracklist component

--- src/components/molecules/SortableTrackTable.tsx
+++ src/components/molecules/SortableTrackTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   DndContext,
   closestCenter,
@@ -21,10 +21,16 @@ import {
   FileInput,
   Flex,
   Space,
+  Loader,
+  Progress,
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useDisclosure } from '@mantine/hooks';
 import { PlusIcon } from '@phosphor-icons/react';
+import { getTracksForBand, reorder } from '../../api/generated';
+import { notifications } from '@mantine/notifications';
+import { Subject, concatMap, finalize, from, tap } from 'rxjs';
+import axios from 'axios';
 
 type AddTrackFormValues = {
   title: string;
@@ -38,14 +44,7 @@ type SortableTrackTableProps = {
 type Item = {
   uuid: string;
   title: string;
-  temp: boolean;
 };
-
-const initialItems: Item[] = [
-  { uuid: '1', title: 'Alpha', temp: false },
-  { uuid: '2', title: 'Beta', temp: false },
-  { uuid: '3', title: 'Gamma', temp: false },
-];
 
 function SortableRow({ item }: { item: Item }) {
   const {
@@ -86,7 +85,37 @@ function SortableRow({ item }: { item: Item }) {
 
 export function SortableTrackTable({ bandId }: SortableTrackTableProps) {
 
-  const [items, setItems] = useState(initialItems);
+  const abortControllerRef = useRef<AbortController>(null);
+
+  const [trackUploadProgress, setTrackUploadProgress] = useState(0);
+  const [uploadingTrack, setUploadingTrack] = useState(false);
+
+  const reorder$ = useRef(new Subject<Item[]>()).current;
+  const [pendingReorderRequests, setPendingOrderRequests] = useState(0);
+  const reordering = pendingReorderRequests > 0;
+
+  const [addTrackFormSubmitDisabled, setAddTrackFormSubmitDisabled] = useState(false);
+
+  useEffect(() => {
+
+    const subscription = reorder$.pipe(
+      tap(() => setPendingOrderRequests(value => value + 1)),
+      concatMap((items) =>
+        from(reorder({ path: { bandId }, body: items })).pipe(
+          finalize(() => setPendingOrderRequests(value => value - 1))
+        )
+      )
+    ).subscribe();
+
+    return () => subscription.unsubscribe();
+
+  }, [reorder$, bandId]);
+
+  useEffect(() => {
+    loadTracklist();
+  }, [bandId]);
+
+  const [items, setItems] = useState<Item[]>([]);
 
   const addTrackForm = useForm({
     mode: 'uncontrolled',
@@ -105,9 +134,70 @@ export function SortableTrackTable({ bandId }: SortableTrackTableProps) {
     close: closeAddTrackModal
   }] = useDisclosure();
 
-  const handleAddTrackSubmit = (event: AddTrackFormValues) => {
-    console.log(event);
+  const handleAddTrackSubmit = async (event: AddTrackFormValues) => {
+
+    abortControllerRef.current = new AbortController();
+
+    setAddTrackFormSubmitDisabled(true);
+    setUploadingTrack(true);
+
+    const formData = new FormData();
+
+    formData.append('title', event.title);
+    formData.append('bandId', bandId.toString());
+    formData.append('audioFile', event.file!);
+
+    setUploadingTrack(true);
+
+    let uploadResult;
+
+    try {
+      uploadResult = await axios.post(
+        `${import.meta.env.VITE_API_URL}/track/add`, formData, {
+        signal: abortControllerRef.current.signal,
+        withCredentials: true,
+        onUploadProgress: (event) => {
+          if (event.total) {
+            const progress = (event.loaded / event.total) * 100;
+            setTrackUploadProgress(progress);
+          }
+        },
+      });
+
+      setAddTrackFormSubmitDisabled(false);
+      setUploadingTrack(false);
+      closeAddTrackModal();
+
+    } catch (e) {
+      if (!axios.isCancel(e)) {
+        notifications.show({
+          title: 'Fehler beim Upload',
+          message: 'Es ist ein Fehler beim Hochladen der Audiodatei aufgetreten.',
+          color: 'red'
+        });
+      }
+
+      setAddTrackFormSubmitDisabled(false);
+      setUploadingTrack(false);
+    }
   }
+
+  const loadTracklist = async () => {
+
+    const trackList = await getTracksForBand({ path: { bandId } });
+
+    if (trackList.status === 200 && trackList.data) {
+      setItems(trackList.data);
+      return;
+    }
+
+    notifications.show({
+      title: 'Fehler',
+      message: 'Es ist ein Fehler beim Laden einer Tracklist aufgetreten.',
+      color: 'red',
+    });
+
+  };
 
   function handleDragEnd(event: DragEndEvent) {
     const { active, over } = event;
@@ -125,8 +215,21 @@ export function SortableTrackTable({ bandId }: SortableTrackTableProps) {
         (item) => item.uuid === over.id
       );
 
-      return arrayMove(currentItems, oldIndex, newIndex);
+      const newArray = arrayMove(currentItems, oldIndex, newIndex);
+
+      reorder$.next(newArray);
+
+      return newArray;
     });
+  }
+
+  const abortAddTrack = () => {
+
+    if (abortControllerRef.current) {
+      abortControllerRef.current.abort();
+    }
+
+    closeAddTrackModal();
   }
 
   return (
@@ -155,17 +258,21 @@ export function SortableTrackTable({ bandId }: SortableTrackTableProps) {
           </Table.Tbody>
         </Table>
       </DndContext>
-      <Box ta='right' mt='md'>
+      <Flex justify='flex-end' align='center' gap='sm'>
+        {reordering && <Loader size={16} mr='auto' />}
         <Button
-          onClick={openAddTrackModal}
+          onClick={() => {
+            addTrackForm.reset();
+            openAddTrackModal();
+          }}
           leftSection={<PlusIcon size={16} />}
         >
           Track hinzufügen
         </Button>
-      </Box>
+      </Flex>
       <Modal
         opened={addTrackModalOpened}
-        onClose={closeAddTrackModal}
+        onClose={abortAddTrack}
         title='Track hinzufügen'
       >
         <form onSubmit={addTrackForm.onSubmit(handleAddTrackSubmit)} style={{ width: '100%' }}>
@@ -181,9 +288,10 @@ export function SortableTrackTable({ bandId }: SortableTrackTableProps) {
             {...addTrackForm.getInputProps('file')}
           />
           <Flex direction='row' align='center' justify='flex-end' mt='xl'>
-            <Button variant='default' onClick={closeAddTrackModal}>Abbrechen</Button>
+            {uploadingTrack && <Progress flex={1} value={trackUploadProgress} mr='md' />}
+            <Button variant='default' onClick={abortAddTrack}>Abbrechen</Button>
             <Space w='sm' />
-            <Button type='submit'>Speichern</Button>
+            <Button type='submit' disabled={addTrackFormSubmitDisabled}>Speichern</Button>
           </Flex>
         </form>
       </Modal>
